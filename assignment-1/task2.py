5d9e817706f746c38d4b34d07c51c08ce4a6b539
Darsh96 <60422811+Darsh96@users.noreply.github.com>
2025-03-25 03:51:08 +0530

    Add files via upload

---
+++ assignment-1/task2.py
@@ -0,0 +1,9 @@
+print("Enter First name and Last name")
+
+fname =input("enter first name")
+
+lname = input("enter last name")
+
+nm = fname + lname
+
+print("Hello " + nm + "! Welcome to Python Program")
